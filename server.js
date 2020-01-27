752650cb0a73a1f5d69f2c94df67734dd9794baa
Paolo_X86 <p.ferri1986@gmail.com>
2020-01-27 10:35:15 +0800

    First commit

---
+++ server.js
@@ -0,0 +1,69 @@
+const server = require('http').createServer()
+const io = require('socket.io')(server)
+
+
+
+class User {
+  constructor(id, username,socket) {
+    this.id = id;
+    this.username = username;
+    this.socket = socket;
+  }
+}
+
+var allClients = new Object();
+
+io.on('connection', function (client) {
+
+
+  
+  client.on('disconnect', function () {    
+    console.log('SERVER: client disconnect...', allClients[client.id].username)
+    delete allClients[client.id]
+    console.log('SERVER: Client deleted, total size ' + Object.keys(allClients).length)
+    
+  })
+
+  client.on('error', function (err) {
+    console.log('SERVER: received error from client:', client.id)
+    console.log(err)
+  })
+
+  client.on('chat-msg', function(msg){
+
+  });
+
+  function CheckConnection(arg) {
+
+    Object.keys(allClients).forEach(function(key) {
+      console.log('SERVER: Connection with ' + allClients[key].username + ': '  +  allClients[key].socket.connected);
+      
+    });
+
+  }
+
+  client.on('intro', function(msg){
+
+    var us = new User();
+    us.id = client.id;
+    us.username = msg;
+    us.socket = client;
+    allClients[client.id] = us;    
+
+    console.log('SERVER: Client ' + us.username +' added, total size ' + Object.keys(allClients).length)
+    client.broadcast.emit('msg','hello from ' + us.username);
+
+   
+    setInterval(CheckConnection,1000,'');
+
+
+
+
+  });
+})
+
+server.listen(3000, function (err) {
+  if (err) throw err
+  console.log('SERVER: listening on port 3000')
+})
+
