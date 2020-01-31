bd0574bad40b6cc6cc3b52f2b11be4e592961e2e
Paolo_X86 <p.ferri1986@gmail.com>
2020-01-31 21:29:09 +0800

    Added live list of usernames

--- public/scripts/users.js
+++ public/scripts/users.js
@@ -3,7 +3,30 @@ const address = "http://localhost:3000/users"
 setInterval(function(){ 
 
     $.get(address, function(data, status){
-      $("#responseContent").html(data)
+
+      var dataObj = JSON.parse(data);
+      var result = ""
+      if(dataObj.usernames == null || dataObj.usernames.length == 0)
+        {
+
+        }
+      else
+        {
+         
+          for (var i=0; i<dataObj.usernames.length; i++) {
+
+            result += `         
+            <tr>
+              <td>${dataObj.ids[i]}</td>
+              <td>${dataObj.usernames[i]}</td>
+            </tr>`
+
+        }
+      }
+
+      $("#usersTable").html(result)
+
+      
   });
   
   
--- requests.js
+++ requests.js
@@ -15,13 +15,15 @@ onUsersRequest: function(req, res, allClients) {
     res.writeHead(200, {'Content-Type': 'text/html'});
 
     var usersArray = []
+    var idArray = []
 
 
     Object.keys(allClients).forEach(function(key) {
         usersArray.push(allClients[key].username)
+        idArray.push(allClients[key].id)
       });
-
-    var result = {'users': usersArray}
+    var result = {'usernames': usersArray}
+    result['ids'] = idArray;
       
     res.write(JSON.stringify(result));
     res.end();   
